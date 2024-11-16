bb8e738e210368c5b77a5da8f24f7757042f832b
Öykü Kaya <oykuky001@gmail.com>
2024-11-16 17:19:52 +0300

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,24 @@
-// import Image from 'next/image'
+"use client";
 import React from "react";
 import SparklesText from "@/components/ui/sparkles-text";
-// import Image from "next/image";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 const Header = () => {
+  const t = useTranslations();
   return (
-    <div className="bg-gray-950 flex justify-center items-center w-3/4 h-28 rounded-2xl border-[4px] border-purple-800 mt-5 mb-10 ">
+    <div className="bg-gray-950 flex flex-col justify-center items-center w-3/4 rounded-2xl border-[3px] border-purple-800 mt-5 mb-10 ">
       <div className="flex flex-col text-center font-bold gap-5">
         <div className="flex justify-center items-center my-5">
           <SparklesText text="Dream Catcher" />
+        </div>
+        <div className="mb-5">
+          <Link
+            href="/dreamLibrary"
+            className="text-white hover:text-purple-400 transition-colors duration-200"
+          >
+            {t("header.mydreams")}
+          </Link>
         </div>
       </div>
     </div>
